deaec36b3a52b9cd8deab15631d21dcb2d558b50
yusufaisal <yusuf@koinworks.com>
2020-05-12 13:22:53 +0800

    refine animation

--- example/AnimatedParalel.js
+++ example/AnimatedParalel.js
@@ -12,9 +12,10 @@ import {
 export default class AnimatedParalel extends Component {
     constructor(props) {
         super(props)
-        this.animatedValue1 = new Animated.Value(0)
-        this.animatedValue2 = new Animated.Value(0)
-        this.animatedValue3 = new Animated.Value(0)
+        this.animatedOpacity = new Animated.Value(0)
+        this.animatedTitle = new Animated.Value(0)
+        this.animatedBody = new Animated.Value(0)
+        this.animatedButton = new Animated.Value(0)
     }
 
     componentDidMount() {
@@ -28,9 +29,10 @@ export default class AnimatedParalel extends Component {
         // }
 
         console.log("is pressed")
-        this.animatedValue1.setValue(0)
-        this.animatedValue2.setValue(0)
-        this.animatedValue3.setValue(0)
+        this.animatedOpacity.setValue(0)
+        this.animatedTitle.setValue(0)
+        this.animatedBody.setValue(0)
+        this.animatedButton.setValue(0)
         const createAnimation = function (value, duration, easing, delay = 0) {
             return Animated.timing(
                 value,
@@ -39,38 +41,44 @@ export default class AnimatedParalel extends Component {
                     duration,
                     easing,
                     delay,
+                    useNativeDriver: true
                 }
             )
         }
         Animated.parallel([
-            createAnimation(this.animatedValue1, 700, Easing.inOut(Easing.sin)),
-            createAnimation(this.animatedValue2, 900, Easing.inOut(Easing.sin)),
-            createAnimation(this.animatedValue3, 1100, Easing.inOut(Easing.sin))
+            createAnimation(this.animatedOpacity, 700, Easing.inOut(Easing.sin)),
+            createAnimation(this.animatedTitle, 700, Easing.inOut(Easing.sin)),
+            createAnimation(this.animatedBody, 900, Easing.inOut(Easing.sin)),
+            createAnimation(this.animatedButton, 1100, Easing.inOut(Easing.sin))
         ]).start()
     }
 
     render() {
-        const introText1 = this.animatedValue1.interpolate({
+        const introOpacity = this.animatedOpacity.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1]
+        })
+        const introText1 = this.animatedTitle.interpolate({
             inputRange: [0, 1],
             outputRange: [-200, 0]
         })
-        const introText2 = this.animatedValue2.interpolate({
+        const introText2 = this.animatedBody.interpolate({
             inputRange: [0, 1],
             outputRange: [-200, 0]
         })
-        const introButton = this.animatedValue3.interpolate({
+        const introButton = this.animatedButton.interpolate({
             inputRange: [0, 1],
             outputRange: [-200, 0]
         })
         return (
             <View style={styles.container}>
-                <Animated.Text style={[styles.title,  {left: introText1}]}>
+                <Animated.Text style={[styles.title,  {opacity:introOpacity ,transform: [{translateX:introText1}]}]}>
                     <Text>Welcome to React Native</Text>
                 </Animated.Text>
-                <Animated.Text style={[styles.body, {left: introText2}]}>
+                <Animated.Text style={[styles.body, {opacity:introOpacity, transform: [{translateX:introText2}]}]}>
                     <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
                 </Animated.Text>
-                <Animated.View style={{left: introButton,}}>
+                <Animated.View style={{opacity:introOpacity, transform: [{translateX:introButton}]}}>
                     <Button
                         title={"Re-Animate"}
                         onPress={this.animate}
@@ -90,12 +98,12 @@ const styles = StyleSheet.create({
     title: {
         fontWeight: "bold",
         fontSize: 19,
-        marginHorizontal: 25,
+        marginHorizontal: 20,
         marginVertical: 5,
     },
     body: {
         fontSize: 17,
-        marginHorizontal: 25,
+        marginHorizontal: 20,
         marginVertical: 5,
 
 
